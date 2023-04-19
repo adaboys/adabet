8c41f63766a58f70b84006b8d2a1686ffbe00639
ADABoys LAB <94250743+adaboys@users.noreply.github.com>
2023-04-19 12:42:22 +0700

    Update 2023_03_03_000000_alter_users_table_add_fields.php
    up

--- casino/database/migrations/2023_03_03_000000_alter_users_table_add_fields.php
+++ casino/database/migrations/2023_03_03_000000_alter_users_table_add_fields.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *   Stake iGaming platform
+ *   Stake ADABET platform
  *   ----------------------
- *   2023_03_03_000000_alter_users_table_add_fields.php
+ *   filename.php
  * 
- *   @copyright  Copyright (c) FinancialPlugins, All rights reserved
- *   @author     FinancialPlugins <info@financialplugins.com>
- *   @see        https://financialplugins.com
+ *   @copyright  Copyright (c) ADABET, All rights reserved
+ *   @author     dev <contact@adabet.io>
+ *   @see        https://adabet.io
 */
 
 use Illuminate\Database\Migrations\Migration;
