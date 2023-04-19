d178e39d481d9cc2c0039bb7d0554a479c90cc37
ADABoys LAB <94250743+adaboys@users.noreply.github.com>
2023-04-19 13:25:49 +0700

    Update UserFields.php

--- casino/app/Casts/UserFields.php
+++ casino/app/Casts/UserFields.php
@@ -1,12 +1,12 @@
 <?php
 /**
- *   Stake iGaming platform
+ *   Stake ADABET platform
  *   ----------------------
- *   UserFields.php
+ *   app/Casts
  * 
- *   @copyright  Copyright (c) FinancialPlugins, All rights reserved
- *   @author     FinancialPlugins <info@financialplugins.com>
- *   @see        https://financialplugins.com
+ *   @copyright  Copyright (c) ADABET, All rights reserved
+ *   @author     dev <contact@adabet.io>
+ *   @see        https://adabet.io
 */
 
 namespace App\Casts;
